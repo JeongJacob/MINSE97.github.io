a3ee228da541fd7fa5e7fed9cf8314d49abe3954
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-04-30 23:04:11 +0900

    Feat: Scroll 감지 시 애니메이션 작동

--- src/app/components/Iam.tsx
+++ src/app/components/Iam.tsx
@@ -1,10 +1,29 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
 import iam from "@/app/components/styles/Iam.module.scss";
 
 const Iam = () => {
+  const [onScroll, setOnScroll] = useState(false);
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          setOnScroll(true);
+        }
+      },
+      { threshold: 1 }
+    );
+    if (sectionRef.current) observer.observe(sectionRef.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
   return (
     <div className={iam.container}>
       <h1>I Am</h1>
-      <hr className={iam.underline}/>
+      <hr className={onScroll ? iam.underline : ""} />
     </div>
   );
 };
