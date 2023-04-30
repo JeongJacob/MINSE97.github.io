880bcef2e5745582910c26733aec93fb8f6816f2
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-04-30 14:31:44 +0900

    Design: Intro TypingText 애니메이션 삽입

--- src/app/components/Intro/Intro.tsx
+++ src/app/components/Intro/Intro.tsx
@@ -12,6 +12,9 @@ const Intro = () => {
           height={1000}
           className={intro.semin}
         />
+        <p className={intro.typingText}>
+          안녕하세요 프론트엔드 개발자 정세민입니다.
+        </p>
       </div>
     </section>
   );
