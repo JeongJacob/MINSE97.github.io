2ddaebd1a5498c22084ed4fa6c3902df89a844dd
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-05-01 18:27:55 +0900

    Design: Projects 레이아웃 설정

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,8 +1,39 @@
-import React from "react";
-import projects from "@/app/components/styles/projects.module.scss";
+import Image from "next/image";
+import projects from "@/app/components/styles/Projects.module.scss";
+import Link from "next/link";
 
 const Projects = () => {
-  return <div></div>;
+  return (
+    <div className={projects.wrapper}>
+      <h1>Projects</h1>
+      <div className={projects.container}>
+        <Link href={"/project/pikcha"}>
+          <div className={projects.logo__wrapper}>
+            <div className={projects.logo__container}>
+              <Image
+                src={"/pikcha.svg"}
+                alt="프로젝트"
+                width={200}
+                height={200}
+              />
+            </div>
+          </div>
+        </Link>
+        <Link href={"/project/nerdnest"}>
+          <div className={projects.logo__wrapper}>
+            <div className={projects.logo__container}>
+              <Image
+                src={"/nerdnest.png"}
+                alt="프로젝트"
+                width={200}
+                height={150}
+              />
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
