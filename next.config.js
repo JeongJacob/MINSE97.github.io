ecb690d9ed423d06015c57455010fd06e5538bd0
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-05-09 01:29:53 +0900

    HOTFIX: Axios get URL 수정

--- next.config.js
+++ next.config.js
@@ -3,6 +3,27 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/api/projects",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://jacob-minse97.vercel.app/", // 모든 출처 허용
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, OPTIONS", // 허용할 HTTP 메서드
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization", // 허용할 헤더
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
