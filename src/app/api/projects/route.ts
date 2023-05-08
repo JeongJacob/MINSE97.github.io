7ec271aada567f8c42989c3f9b312acff6f491da
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-05-09 00:51:43 +0900

    HOTFIX: api route 설정

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,9 +1,36 @@
-import { connectDB } from "../../../../util/database";
+const project = [
+  {
+    name: "pikcha",
+    skills: ["React.js", "TypeScript", "Recoil", "Styled-Components", "Axios"],
+    github: "https://github.com/codestates-seb/seb41_main_036",
+    link: "https://pikcha36.o-r.kr",
+    adminID: "admin@gmail.com",
+    adminPW: "@seb!main36",
+  },
+  {
+    name: "nerdnest",
+    skills: [
+      "React.js",
+      "TypeScript",
+      "Toast UI Viewer",
+      "Styled-Components",
+      "Axios",
+    ],
+    github: "https://github.com/limdumb/NerdNest_FE",
+    link: "http://nerdnest.s3-website.ap-northeast-2.amazonaws.com/",
+    adminID: "admin@gmail.com",
+    adminPW: "@seb!main36",
+  },
+  {
+    name: "portfolio",
+    skills: ["Next.js", "TypeScript", "Scss", "Axios"],
+    github: "https://github.com/MINSE97/MINSE97.github.io",
+    link: "https://jacob-minse97.vercel.app/",
+  },
+];
 
 export async function GET(request: Request) {
-  const db = (await connectDB()).db("forum");
-  const result = await db.collection("projects").find().toArray();
-  const responseBody = JSON.stringify(result);
+  const responseBody = JSON.stringify(project);
   try {
     return new Response(responseBody, {
       status: 200,
@@ -15,5 +42,6 @@ export async function GET(request: Request) {
     });
   } catch (error) {
     console.error(error);
+    return new Response("Error occurred", { status: 500 });
   }
 }
