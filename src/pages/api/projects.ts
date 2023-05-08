14af4f17da4478fddb82eea81e387149e7aabaf0
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-05-08 21:50:06 +0900

    HOTFIX: Server 파일 수정

--- src/pages/api/projects.ts
+++ src/pages/api/projects.ts
@@ -1,45 +1,30 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import { connectDB } from "@/../util/database";
-const express = require("express");
-const cors = require("cors");
+import { NextApiRequest, NextApiResponse } from "next";
+import express from "express";
+import cors from "cors";
+
 const app = express();
 
 export default async function nerdNestProject(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  app.use(cors());
-  const allowedOrigins = [
-    "https://jacob-minse97.vercel.app/",
-    "http://localhost:3000",
-  ];
-
   const corsOptions = {
-    origin: function (origin: any, callback: any) {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: ["https://jacob-minse97.vercel.app", "http://localhost:3000"],
+    methods: ["GET", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    preflightContinue: false,
   };
 
   app.use(cors(corsOptions));
+  app.options("/api/projects", cors(corsOptions));
 
-  // ... your other routes and middleware
-
-  app.listen(3000, () => {
-    console.log("Server listening on port 3000");
-  });
-  if (connectDB) {
-    let db = (await connectDB).db("forum");
-    let result = await db.collection("projects").find().toArray();
-    if (req.method === "GET") {
-      try {
-        res.status(200).json(result);
-      } catch {
-        res.status(404).json("404 not found");
-      }
-    }
+  try {
+    const db = (await connectDB()).db("forum");
+    const result = await db.collection("projects").find().toArray();
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 }
--- util/database.ts
+++ util/database.ts
@@ -2,18 +2,13 @@ import { MongoClient } from "mongodb";
 
 const url = process.env.NEXT_PUBLIC_MONGODB_URI;
 let mongoClient: MongoClient | null = null;
-let connectDB: Promise<MongoClient> | null = null;
 
-if (process.env.NODE_ENV === "development") {
+async function connectDB(): Promise<MongoClient> {
   if (!mongoClient && url) {
     mongoClient = new MongoClient(url);
-    connectDB = mongoClient.connect();
+    await mongoClient.connect();
   }
-} else {
-  if (url) {
-    mongoClient = new MongoClient(url);
-    connectDB = mongoClient.connect();
-  }
+  return mongoClient!;
 }
 
 export { connectDB };
