71241bf4f407250e36499eee5359a41f62cfc1ac
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-05-08 14:45:09 +0900

    HOTFIX: connectDB 수정

--- util/database.ts
+++ util/database.ts
@@ -1,15 +1,20 @@
 import { MongoClient } from "mongodb";
+
 const url = process.env.NEXT_PUBLIC_MONGODB_URI;
 const options = { useNewUrlParser: true };
-let connectDB: Promise<MongoClient>;
+let mongoClient: MongoClient | null = null;
+let connectDB: Promise<MongoClient> | null = null;
 
 if (process.env.NODE_ENV === "development") {
-  if (!global._mongo && url) {
-    global._mongo = new MongoClient(url, options).connect();
+  if (!mongoClient && url) {
+    mongoClient = new MongoClient(url, options);
+    connectDB = mongoClient.connect();
   }
-  connectDB = global._mongo;
 } else {
-  if (url) connectDB = new MongoClient(url, options).connect();
+  if (url) {
+    mongoClient = new MongoClient(url, options);
+    connectDB = mongoClient.connect();
+  }
 }
 
 export { connectDB };
