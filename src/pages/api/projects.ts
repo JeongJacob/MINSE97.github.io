7dddd48a58841018fa38b47e8f038ee7944dce26
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-05-08 14:47:55 +0900

    HOTFIX: API 조건 추가

--- src/pages/api/projects.ts
+++ src/pages/api/projects.ts
@@ -5,13 +5,15 @@ export default async function nerdNestProject(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  let db = (await connectDB).db("forum");
-  let result = await db.collection("projects").find().toArray();
-  if (req.method === "GET") {
-    try {
-      res.status(200).json(result);
-    } catch {
-      res.status(404).json("404 not found");
+  if (connectDB) {
+    let db = (await connectDB).db("forum");
+    let result = await db.collection("projects").find().toArray();
+    if (req.method === "GET") {
+      try {
+        res.status(200).json(result);
+      } catch {
+        res.status(404).json("404 not found");
+      }
     }
   }
 }
